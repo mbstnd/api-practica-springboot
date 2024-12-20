1f619356f4ab9baf32bf78804c4ac944f93fd749
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-19 22:00:43 -0300

    feat: Refactor Profesor model to include supervisor details and list of supervised students

--- src/main/java/ipss/cl/registropracticas/models/Profesor.java
+++ src/main/java/ipss/cl/registropracticas/models/Profesor.java
@@ -1,5 +1,6 @@
 package ipss.cl.registropracticas.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,16 +13,11 @@ import lombok.Data;
 public class Profesor {
 
   @Id
-  private String id; // Identificador único del profesor
-
-  private String nombre;
-  private String email;
+  private String id;
+  private String nombreCompleto;
+  private String departamento;
+  private String correo;
   private String telefono;
-
-  // Relación con las prácticas supervisadas
-  private List<String> practiceIds; // Lista de IDs de prácticas supervisadas
-
-  // Clase interna que actúa como una vista simplificada del profesor
-  private ProfesorSupervisor profesorSupervisor;
+  private List<EstudianteResumen> estudiantesSupervisados = new ArrayList<>();
 
 }
