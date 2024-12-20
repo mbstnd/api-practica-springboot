e1acf145821cbe0f658fe1a60b3e41ed4dadae16
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-19 21:49:18 -0300

    feat: Update EstudianteRepository to use nombreCompleto and simplify query methods

--- src/main/java/ipss/cl/registropracticas/repositories/EstudianteRepository.java
+++ src/main/java/ipss/cl/registropracticas/repositories/EstudianteRepository.java
@@ -8,13 +8,8 @@ import ipss.cl.registropracticas.models.Estudiante;
 
 public interface EstudianteRepository extends MongoRepository<Estudiante, String> {
 
-  // Método para encontrar estudiantes por nombre
-  List<Estudiante> findByNombre(String nombre);
-
-  // Método para encontrar estudiantes por carrera
   List<Estudiante> findByCarrera(String carrera);
 
-  // Método para buscar estudiantes por nombre y carrera
-  List<Estudiante> findByNombreAndCarrera(String nombre, String carrera);
+  List<Estudiante> findByNombreCompleto(String nombreCompleto);
 
 }
