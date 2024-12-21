e4456793effdc4aebd9d58b3fa9bd70cedb142c6
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-21 16:56:38 -0300

    feat: Update PracticaResponse to support multiple practices

--- src/main/java/ipss/cl/registropracticas/responses/PracticaResponse.java
+++ src/main/java/ipss/cl/registropracticas/responses/PracticaResponse.java
@@ -1,15 +1,14 @@
 package ipss.cl.registropracticas.responses;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 
 import ipss.cl.registropracticas.models.Practica;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PracticaResponse {
-  private int status; // Código de estado (por ejemplo, 200, 400, 500)
+  private int status;
   private String message;
-  private Practica practica;
+  private List<Practica> practica;
 
 }
