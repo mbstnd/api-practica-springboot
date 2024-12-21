6d42f57e69b8c6d4f05fa724ad1fc747ac2817ff
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-21 16:54:54 -0300

    feat: Update ProfesorController to use RestController and refine API routes

--- src/main/java/ipss/cl/registropracticas/controllers/ProfesorController.java
+++ src/main/java/ipss/cl/registropracticas/controllers/ProfesorController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import ipss.cl.registropracticas.models.Profesor;
 import ipss.cl.registropracticas.responses.ProfesorResponse;
 import ipss.cl.registropracticas.services.ProfesorService;
 
-@Controller
+@RestController
 @RequestMapping("api/profesores")
 public class ProfesorController {
   @Autowired
