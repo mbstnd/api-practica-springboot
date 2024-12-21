f279cd265b2899492ae22078a294cda3bbbc15a0
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-21 16:55:54 -0300

    fix: Remove compound index from Practica model to address schema constraints issue

--- src/main/java/ipss/cl/registropracticas/models/Practica.java
+++ src/main/java/ipss/cl/registropracticas/models/Practica.java
@@ -2,7 +2,6 @@ package ipss.cl.registropracticas.models;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -12,7 +11,6 @@ import lombok.Data;
 
 @Data
 @Document(collection = "practicas")
-@CompoundIndex(name = "unique_practica_index", def = "{'fechaInicio': 1, 'fechaTermino': 1, 'empresa.id': 1}", unique = true)
 public class Practica {
 
   @Id
