63b7a434b62e5ad3ececba82d8b4a5e8e86ba015
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-21 16:52:34 -0300

    fix: Resolve serialization issue by removing JsonInclude annotation from ProfesorResponse

--- src/main/java/ipss/cl/registropracticas/responses/ProfesorResponse.java
+++ src/main/java/ipss/cl/registropracticas/responses/ProfesorResponse.java
@@ -1,13 +1,11 @@
 package ipss.cl.registropracticas.responses;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import ipss.cl.registropracticas.models.Profesor;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProfesorResponse {
-  private int status; // Código de estado (por ejemplo, 200, 400, 500)
+  private int status;
   private String message;
   private Profesor profesor;
 }
